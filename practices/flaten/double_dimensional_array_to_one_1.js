66195a17a32493e71131a82a4d1dea30a208662e
wyb1995 <wy_b1995@163.com>
2016-05-18 17:14:43 +0800

    x

--- practices/flaten/double_dimensional_array_to_one_1.js
+++ practices/flaten/double_dimensional_array_to_one_1.js
@@ -2,6 +2,9 @@
 
 function double_to_one(collection) {
   var x = collection.join(",").split(",");
+  for(var k = 0;k< x.length;k++){
+    x[k] = Number(x[k]);
+  }
   return x;
   //在这里写入代码
 }
--- practices/flaten/double_dimensional_array_to_one_2.js
+++ practices/flaten/double_dimensional_array_to_one_2.js
@@ -4,6 +4,9 @@ function double_to_one(collection) {
 
   //在这里写入代码
   var x = collection.join(",").split(",");
+  for(var k = 0;k< x.length;k++){
+    x[k] = Number(x[k]);
+  }
   var tmp = new Array();
   for(var i in x) {
     if(tmp.indexOf(x[i])===-1){
